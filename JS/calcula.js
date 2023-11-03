ef0492f4e7a14331a68a42f303fe00aa70ac8ebe
Daniel <dany.relojero@gmail.com>
2023-11-03 20:18:23 -0300

    Listo entrega parte JavaScript

--- JS/calcula.js
+++ JS/calcula.js
@@ -1,3 +1,38 @@
+var botonResumen = document.getElementById("botonCalcular");
+botonResumen.addEventListener('click', verificarDatos);
+
+function verificarDatos()
+{
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var correo = document.getElementById("correo").value;
+    var cantidad = parseInt(document.getElementById("cantidad").value);
+    if((((nombre != "") && (apellido != "")) && (correo != "")) && !Number.isNaN(cantidad))
+    {
+        todosLosCamposCorrectos();
+        calcular();
+    }
+    else
+    {
+        if(nombre == "")document.getElementById("nombre").classList.add("faltanDatos");
+        if(apellido == "")document.getElementById("apellido").classList.add("faltanDatos");
+        if(correo == "")document.getElementById("correo").classList.add("faltanDatos");
+        if(Number.isNaN(cantidad)) document.getElementById("cantidad").classList.add("faltanDatos");
+        
+        //var x = document.getElementById("nombre");
+        //x.classList.add("faltanDatos");
+        alert("FALTAN DATOS!");
+    }
+}
+
+function todosLosCamposCorrectos()
+{
+    document.getElementById("nombre").classList.add("todoBien");
+    document.getElementById("apellido").classList.add("todoBien");
+    document.getElementById("correo").classList.add("todoBien");
+    document.getElementById("cantidad").classList.add("todoBien");
+    document.getElementById("categoria").classList.add("todoBien");
+}
 function calcular()
 {
     var cantidad = parseInt(document.getElementById("cantidad").value);
@@ -21,6 +56,21 @@ function calcular()
             totalAPagar = totalSinDescuento - (totalSinDescuento * descuentoJunior);        
             break;
     }
-    
     document.getElementById("debePagar").value="Total a pagar: $" + totalAPagar;
+
+}
+function redireccionarLosOradores()
+{
+    window.location.href = "#convertirteEnOrador";
+}
+function redireccionarComprarTickets()
+{
+    window.location.href = "compraTickets.html";
+}
+function conoceMas()
+{
+    alert("Muchísima mas informacion...");
+}
+function Enviar(){
+    alert("Enviando...");
 }
